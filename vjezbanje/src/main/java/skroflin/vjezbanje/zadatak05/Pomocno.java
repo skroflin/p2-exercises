c885bc3434b3bc05ecb7ddf46faf1ec8e7883a4e
Sven Kroflin (jer je profesor prigovarao na imena) <sven.kroflin@gmail.com>
2025-04-03 09:40:51 +0200

    dodane metode u pomocnoj klasi i metode za ucitavanje
    dodane metode unutar pomocne klase za ucitavanje stringa i datea, kreirane metode za ucitavanje djece i liste bolesti.

--- vjezbanje/src/main/java/skroflin/vjezbanje/zadatak05/Pomocno.java
+++ vjezbanje/src/main/java/skroflin/vjezbanje/zadatak05/Pomocno.java
@@ -4,6 +4,9 @@
  */
 package skroflin.vjezbanje.zadatak05;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -12,4 +15,37 @@ import java.util.Scanner;
  */
 class Pomocno {
     public static Scanner ulaz;
+
+    static String ucitajString(String poruka) {
+        String s;
+        while (true) {            
+            System.out.println(poruka);
+            s = Pomocno.ulaz.nextLine();
+            if (!s.trim().isBlank()) {
+                return s;
+            }
+            System.out.println("Ovo je obavezan unos!");
+        }
+    }
+
+    static Date ucitajDate(String poruka) {
+        SimpleDateFormat sdf = new SimpleDateFormat(
+        "dd.MM.yyyy.", Locale.of("hr", "HR")
+        );
+        Date d;
+        String s;
+        for (;;){
+            try{
+                System.out.println(poruka);
+                s = Pomocno.ulaz.nextLine();
+                d = sdf.parse(s);
+                if(sdf.format(d).equals(s)){
+                    return d;
+                }
+                System.out.println("Neispravan datum");
+            }catch(Exception e){
+                System.out.println("Unos datuma je obavezan unos!");
+            }
+        }
+    }
 }
--- vjezbanje/src/main/java/skroflin/vjezbanje/zadatak05/Start.java
+++ vjezbanje/src/main/java/skroflin/vjezbanje/zadatak05/Start.java
@@ -17,5 +17,39 @@ public class Start {
     public Start(){
         djeca = new ArrayList<>();
         Pomocno.ulaz = new Scanner(System.in);
+        ucitajDjecu();
+        Pomocno.ulaz.close();
+    }
+    
+    private Bolest ucitajBolest(){
+        System.out.println("Unos bolesti:");
+        Bolest b = new Bolest();
+        b.setNazivBolesti(Pomocno.ucitajString("Unesite naziv bolesti:"));
+        b.setSimptomiBolesti(Pomocno.ucitajString("Unesite simptome bolesti:"));
+        return b;
+    }
+    
+    private List<Bolest> ucitajBolesti(){
+        List<Bolest> bolesti = new ArrayList<>();
+        do {            
+            bolesti.add(ucitajBolest());
+        } while (!Pomocno.ucitajString("Unesite slovo k za prekid unosa bolesti").equalsIgnoreCase("k"));
+        return bolesti;
+    }
+    
+    private void ucitajDijete(){
+        System.out.println("Unos novog djeteta:");
+        Dijete d = new Dijete();
+        d.setIme(Pomocno.ucitajString("Unesite ime djeteta:"));
+        d.setPrezime(Pomocno.ucitajString("Unesite prezime djeteta:"));
+        d.setDatumRodenja(Pomocno.ucitajDate("Unesite datum rođenja djeteta:"));
+        d.setBolesti(ucitajBolesti());
+        djeca.add(d);
+    }
+    
+    private void ucitajDjecu(){
+        do {            
+            ucitajDijete();
+        } while (!Pomocno.ucitajString("Unesite slovo k za prekid unosa djece").equalsIgnoreCase("k"));
     }
 }
